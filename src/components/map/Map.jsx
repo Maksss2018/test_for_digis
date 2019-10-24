d198e82517ea53562b54bd693e18f352461cde2c
MaxMal <malyimaksym37@gmail.com>
2019-10-24 12:36:14 +0300

    markers in the state and on the screen

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {Map as GoogleMap , InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
@@ -7,33 +7,29 @@ import "./scss/index.scss"
 const  { REACT_APP_MAP_KEY:key }= process.env;
 
 const Map = ({google, ...props}) => {
+    const [marks,setMarks] = useState([]);
     const onMarkerClick = (e) => console.dir(e);
     const onInfoWindowClose = (e) => console.dir(e);
     //e.google.maps.Geocoder.geocode()GeocoderRequest // to get LtgLng
     const handleMarker = (t, map, coords) =>{
-        let lat = coords.latLng.lat(),
-            lng = coords.latLng.lng();
-        console.dir({lat,lng, map })
+        const parsedCoords ={
+            lat : coords.latLng.lat(),
+            lng : coords.latLng.lng(),
+        };
+
+        setMarks([...marks, parsedCoords]);
         map.panTo(coords.latLng)
     };
-
     const putMarker =  (e) => console.dir(e);
-    //const =google;
-
     return (<MainJsx  onMarkerClick={onMarkerClick}
                       onInfoWindowClose={onInfoWindowClose}
                       putMarker={handleMarker}
+                      coordsArr={marks}
                       google={google}
                       {...props}/>);
 }
-/*
-*
-                  <Marker onClick={onMarkerClick}
-                          name={'Current location'} />
 
-*
-* */
-const MainJsx = ({google,onMarkerClick,onInfoWindowClose,putMarker}) => (
+const MainJsx = ({google,onMarkerClick,onInfoWindowClose,putMarker,coordsArr}) => (
     <GoogleMap
                   zoom={14}
                   google={google}
@@ -45,7 +41,15 @@ const MainJsx = ({google,onMarkerClick,onInfoWindowClose,putMarker}) => (
                       display: "inline-block",
                       overflow: "auto",
                   }}
+
     >
+                   {
+                       coordsArr.map((coords)=>(<Marker
+                               key={Object.values(coords).reduce((acc,next) => `${acc}${next}`)}
+                               position={{...coords}}
+                               name={'Current location'} />
+                       ))
+                   }
                   <InfoWindow onClose={onInfoWindowClose}>
                       <div>
                           <h1>!!!!</h1>
