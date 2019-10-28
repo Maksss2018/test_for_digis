2a6578bedb15d6254b782aedd4162b5d0f76854b
MaxMal <malyimaksym37@gmail.com>
2019-10-28 09:03:59 +0200

    with prettier

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -1,51 +1,48 @@
 /* eslint-disable no-console */
-const jsonServer = require("json-server")
-const server = jsonServer.create()
-const path = require("path")
-const router = jsonServer.router(path.join(__dirname, "db.json"))
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
-const middlewares = jsonServer.defaults()
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 server.use(function(req, res, next) {
-  setTimeout(next, 0)
-})
+  setTimeout(next, 0);
+});
 
 server.use((req, res, next) => {
-  if (req.method === "POST") {
-    req.body.createdAt = Date.now()
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
   }
-  next()
-})
+  next();
+});
 
-
-
-server.post("/users/", function(req, res, next) {
+server.post('/users/', function(req, res, next) {
   if (error) {
-    res.status(400).send(error)
+    res.status(400).send(error);
   } else {
-    req.body.id = req.body.token
-    next()
+    req.body.id = req.body.token;
+    next();
   }
+});
 
-})
+server.use(router);
 
-server.use(router)
-
-const port = 3001
+const port = 3001;
 server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`)
-})
+  console.log(`JSON Server is running on port ${port}`);
+});
 
 function createSlug(value) {
   return value
-    .replace(/[^a-z0-9_]+/gi, "-")
-    .replace(/^-|-$/g, "")
-    .toLowerCase()
+    .replace(/[^a-z0-9_]+/gi, '-')
+    .replace(/^-|-$/g, '')
+    .toLowerCase();
 }
 
-function validate({token}) {
-  if (!token) return "token unset"
-  return ""
+function validate({ token }) {
+  if (!token) return 'token unset';
+  return '';
 }
